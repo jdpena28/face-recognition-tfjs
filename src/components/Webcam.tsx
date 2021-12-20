7236ffec08035f2f486b31f860a26699c08a492a
jdpena28 <johnhenrich1128@gmail.com>
2021-12-20 15:24:31 +0800

    Add imports for manifest

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -1,14 +1,33 @@
-import React, { useRef } from 'react'
-import Webcam from 'react-webcam'
-const Webcamtsx = () => {
-    const webcamRef = useRef(null)
-    return (
-        <div className='w-full'>
-            <Webcam className='mx-auto mt-3'
-            audio={false}
-            mirrored={true} ref={webcamRef}/>
-        </div>
-    )
-}
+import React, { useEffect, useRef, useState } from "react";
+import * as faceapi from "face-api.js";
 
-export default Webcamtsx
+const Webcamtsx = () => {
+  const webcamRef = useRef<any | null>(null);
+  const canvasRef = useRef<any | null>(null);
+
+  useEffect(() => {
+    const loadModels = async () => {
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+      ])
+    }
+    loadModels();
+  }, [])
+
+  
+
+  return (
+    <div className="w-full">
+      <video autoPlay width={400} height={400} muted ref={webcamRef} />
+      <canvas
+        ref={canvasRef}
+        className="w-[600px] h-[450px] my-auto z-20 absolute inset-0 mx-auto"
+      />
+    </div>
+  );
+};
+
+export default Webcamtsx;
