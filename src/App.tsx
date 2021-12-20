6da2c7e5d095ca1db01e38e39cb056cf57c836df
jdpena28 <johnhenrich1128@gmail.com>
2021-12-20 10:32:58 +0800

    Initialize repository for face-recognition

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+  <section className="w-full h-screen bg-[#e1dfe1] text-gray-800">
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-center pt-4">Facial Recognition</h1>
+    </div>
+    
+  </section>
+  )
+}
+
+export default App
